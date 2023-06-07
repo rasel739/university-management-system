be36de3603db575ba64499303f6782392d719078
rasel739 <raselhossaintro@gmail.com>
2023-06-08 01:10:05 +0600

    [Done] error handling and code optimize is done

--- src/app/modules/users/users.controller.ts
+++ src/app/modules/users/users.controller.ts
@@ -1,18 +1,23 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { UserService } from './users.service';
 import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import httpStatus from 'http-status';
 
-const createUserController = catchAsync(async (req: Request, res: Response) => {
-  const { user } = req.body;
+const createUserController = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { user } = req.body;
 
-  const result = await UserService.createUserService(user);
-
-  res.status(200).json({
-    sucess: true,
-    message: 'user created successfully',
-    data: result,
-  });
-});
+    const result = await UserService.createUserService(user);
+    next();
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'user created successfully',
+      data: result,
+    });
+  }
+);
 
 export const UserController = {
   createUserController,
