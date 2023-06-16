9fb3c29b8962196efb01d768e4cbd3bef3aa9178
rasel739 <raselhossaintro@gmail.com>
2023-06-17 01:24:20 +0600

    [Done] student module is completed

--- src/app/modules/student/student.constent.ts
+++ src/app/modules/student/student.constent.ts
@@ -12,3 +12,21 @@ export const bloodGroup: IBloodGroup[] = [
   'O+',
   'O-',
 ];
+
+export const studentSearchableFields = [
+  'id',
+  'email',
+  'contactNo',
+  'name.fisrtName',
+  'name.middleName',
+  'name.lastName',
+];
+
+export const studentFilterableFields = [
+  'searchTerm',
+  'id',
+  'bloodGroup',
+  'email',
+  'contactNo',
+  'emergencyContactNo',
+];
--- src/app/modules/users/users.controller.ts
+++ src/app/modules/users/users.controller.ts
@@ -1,23 +1,25 @@
 import { Request, Response } from 'express';
-import { UserService } from './users.service';
+import { RequestHandler } from 'express-serve-static-core';
+import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
-import httpStatus from 'http-status';
 import { IUser } from './users.interface';
+import { UserService } from './users.service';
 
-const createUserController = catchAsync(async (req: Request, res: Response) => {
-  const { ...userData } = req.body;
+const createStudent: RequestHandler = catchAsync(
+  async (req: Request, res: Response) => {
+    const { student, ...userData } = req.body;
+    const result = await UserService.createStudent(student, userData);
 
-  const result = await UserService.createUserService(userData);
-
-  sendResponse<IUser>(res, {
-    statusCode: httpStatus.OK,
-    success: true,
-    message: 'user created successfully',
-    data: result,
-  });
-});
+    sendResponse<IUser>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'user created successfully!',
+      data: result,
+    });
+  }
+);
 
 export const UserController = {
-  createUserController,
+  createStudent,
 };
